c041e42d395c18144ac9f1d819cc64d6b301a2c2
uguroban <36131157+uguroban@users.noreply.github.com>
2022-10-24 12:05:37 +0300

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+console.log('Client side javascript file is loadded')
+
+// fetch('https://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
+
+
+
+const weatherForm=document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#p1')
+const messageTwo = document.querySelector('#p2')
+
+
+weatherForm.addEventListener('submit',(e) => {
+    e.preventDefault()
+
+    const location = search.value
+    console.log=location.valueOf
+
+    fetch('http://localhost:3000/weather?address='+location).then((response) => {response.json().then((data) => {
+        if(data.error){
+            console.log(data.error)
+            messageOne.textContent=data.error
+        }else{
+            messageOne.textContent=data.location
+            messageTwo.textContent=data.forecast
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,11 @@
+const request = require('request')
+
+const forecast = (X,Y,callback) => {
+    const url='http://api.weatherstack.com/current?access_key=8e0c04d566184dae5daea2ab67e0edee&query='+X+','+Y+''
+
+    request({ url: url, json: true }, (error, response) => {
+        console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degress out. It feels like ' + response.body.current.feelslike + ' degress.')
+    })
+}
+
+module.exports = forecast
