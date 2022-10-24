e8aeae41dc23361bb4001414d0bb043742deb2f7
uguroban <36131157+uguroban@users.noreply.github.com>
2022-10-24 17:09:32 +0300

    add try catch geocode

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -6,20 +6,27 @@ const geocode = (address, callback) => {
     const url ='https://api.mapbox.com/geocoding/v5/mapbox.places/'+address+'.json?access_token=pk.eyJ1IjoidWd1cm9iYW4iLCJhIjoiY2w5a2QwZGplMGF0NDN3cWRkZGU1a29kMiJ9.93iltKVwOjacZTEX8_I5hQ&limit=1'
 
     request({url, json:true}, (error,{body}) => {
-        if(error){
-            callback('Unable to connect to location services',undefined)
-         }
-         else if(body.features.length === 0){
-            callback('Unable to find location.Try another search!',undefined)
-         }
-         else{
-           callback(undefined,{
-                latitude: body.features[0].center[1],
-                longitude:body.features[0].center[0],
-                location: body.features[0].place_name
+     try {
 
-           })
-         }
+      callback(undefined,{
+        latitude: body.features[0].center[1],
+        longitude:body.features[0].center[0],
+        location: body.features[0].place_name
+
+   })
+      
+     } catch (error) {
+     if(error){
+        callback('Unable to connect to location services',undefined)
+     }
+     else(body.features.length === 0){
+        callback('Unable to find location.Try another search!',undefined)
+     }
+     
+     callback('Error: '+error,undefined)
+     }
+        
+        
     })
 }
 
