14a6b10d55e23227110684db9853706ae1a5cb64
uguroban <36131157+uguroban@users.noreply.github.com>
2022-10-24 16:50:51 +0300

    js and style changed

--- public/js/app.js
+++ public/js/app.js
@@ -18,18 +18,18 @@ weatherForm.addEventListener('submit',(e) => {
     e.preventDefault()
 
     const location = search.value
-    
 
-    fetch('/weather?address='+location).then((response) => {response.json().then((data) => {
-        if(data.error){
-            console.log(data.error)
-            messageOne.textContent=data.error
-        }else{
-            messageOne.textContent=data.location
-            messageTwo.textContent=data.forecast
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
     })
-})
 })
